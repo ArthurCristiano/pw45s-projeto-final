192677fff5d2d67fc608ad05b05bcbae70622af6
Arthur C <arthurcristiano248@gmail.com>
2025-12-07 20:25:03 -0300

    Migração para PostgreSQL
    [*] Adicionado dependências para o uso do PostgreSQL
    [*] Adicionado SqlTypes.VARBINARY, porque estava criando o campo no banco como OID, agora fica como bytea;

--- server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/OrderAttachment.java
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/OrderAttachment.java
@@ -2,6 +2,8 @@ package br.edu.utfpr.pb.pw44s.server.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "tb_order_attachments")
@@ -19,6 +21,7 @@ public class OrderAttachment {
 
     @Lob
     @Column(length = 10000000)
+    @JdbcTypeCode(SqlTypes.VARBINARY)
     private byte[] data;
 
     @ManyToOne(fetch = FetchType.LAZY)
